7c1cc5d5e6df3762ee8395f0926c8103948115da
Petrick0255-create <parkjunbum0255@gmail.com>
2026-06-21 22:23:49 +0900

    ㄱㄱㄱ

--- js/firebase.js
+++ js/firebase.js
@@ -5,48 +5,73 @@ import {
   collection,
   addDoc,
   getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
   serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAx7c2vMVzsPtMmlT1-J9UcF92J_sKnRYE",
-  authDomain: "blance-hayeon-rankings.firebaseapp.com",
-  projectId: "blance-hayeon-rankings",
-  storageBucket: "blance-hayeon-rankings.firebasestorage.app",
-  messagingSenderId: "866494747318",
-  appId: "1:866494747318:web:da1bf6be56cdae85c38c17"
+  apiKey: "AIzaSyCcFiOtX9S9dTdwoqyfa-bPF4hvJpYGOhs",
+  authDomain: "ranking1-929f6.firebaseapp.com",
+  projectId: "ranking1-929f6",
+  storageBucket: "ranking1-929f6.firebasestorage.app",
+  messagingSenderId: "917283988243",
+  appId: "1:917283988243:web:b7e4e4304331b09406b0e6"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export function getWeekKey() {
+function getWeekKey() {
   const now = new Date();
-  const start = new Date(now.getFullYear(), 0, 1);
-  const day = Math.floor((now - start) / 86400000) + 1;
-  const week = Math.ceil(day / 7);
+
+  const firstDay = new Date(now.getFullYear(), 0, 1);
+
+  const dayOfYear =
+    Math.floor((now - firstDay) / 86400000) + 1;
+
+  const week =
+    Math.ceil(dayOfYear / 7);
+
   return `${now.getFullYear()}-W${week}`;
 }
 
-export async function saveScore({ score, stageId, characterId, name = "" }) {
-  await addDoc(collection(db, "rankings"), {
-    score: Number(score),
-    stageId,
-    characterId,
-    name,
-    weekKey: getWeekKey(),
-    createdAt: serverTimestamp()
-  });
+export async function saveScore({
+  score,
+  stageId,
+  characterId,
+  name = ""
+}) {
+
+  await addDoc(
+    collection(db, "clown-rankings"),
+    {
+      score: Number(score),
+      stageId,
+      characterId,
+      name,
+      weekKey: getWeekKey(),
+      createdAt: serverTimestamp()
+    }
+  );
 }
 
 export async function getTop5(stageId) {
-  const weekKey = getWeekKey();
-  const snapshot = await getDocs(collection(db, "rankings"));
 
-  return snapshot.docs
-    .map(doc => doc.data())
-    .filter(item => item.stageId === stageId)
-    .filter(item => item.weekKey === weekKey)
-    .sort((a, b) => Number(b.score) - Number(a.score))
-    .slice(0, 5);
+  const q = query(
+    collection(db, "clown-rankings"),
+    where("weekKey", "==", getWeekKey()),
+    where("stageId", "==", stageId),
+    orderBy("score", "desc"),
+    limit(5)
+  );
+
+  const snapshot = await getDocs(q);
+
+  return snapshot.docs.map(doc => ({
+    id: doc.id,
+    ...doc.data()
+  }));
 }
